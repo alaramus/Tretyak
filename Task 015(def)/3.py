c852cc410e92ac97c7bcebd729d5e9689d4c727e
alaramus <51889514+alaramus@users.noreply.github.com>
2023-11-16 23:54:07 +0200

    Первый урок

---
+++ Task 015(def)/3.py
@@ -0,0 +1,33 @@
+# Calc
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+while True:
+    choice = input("Выберите действие (+ - * /): ")            # Почему тут ???
+    if choice in ('+', '-', '*', '/'):
+        num1 = float(input("Введите первое число: "))
+        num2 = float(input("Введите второе число: "))
+        if choice == '+':
+            print(num1, "+", num2, "=", add(num1, num2))
+        elif choice == '-':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+        elif choice == '*':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+        elif choice == '/':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    else:
+        print("Неправильный ввод")
