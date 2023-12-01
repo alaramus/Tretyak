61c16d8d67f69acf6745e52e45be02182f9c14bb
alaramus <51889514+alaramus@users.noreply.github.com>
2023-12-01 18:18:21 +0200

    Первый урок

--- Project 001/16.py
+++ Project 001/16.py
@@ -1,11 +1,22 @@
 # сгенерировать  список фибоначе
-def fibonacci(n):
+def fibonacci(num):
     sequence = [0, 1]                                           #список, который начинается с первых двух чисел фибоначчи
-    while len(sequence) < n:
+    while len(sequence) < num:
         next_number = sequence[-1] + sequence[-2]
         sequence.append(next_number)
     return sequence
+def number(num):
+    current_index = 1
+    arr2 = []
+    while current_index <= num:
+        arr2.append(current_index)
+        current_index = current_index + 1
+    return arr2
 
 
-fibonacci_list = fibonacci(11)
-print(fibonacci_list)
+num = int(input('Введите количество элементов числовой последовательности: '))
+
+fibonacci_list = fibonacci(num)
+
+print('Элементы числовой последовательности: ', fibonacci_list)
+print('Порядковый номер в последовательности:',number(num))
